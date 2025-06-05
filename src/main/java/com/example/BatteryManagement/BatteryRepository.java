060853ba4620433f98d70ab032dab41eb933047d
Caspar-Brantberger <3tomu24.branca@folkuniversitetet.nu>
2025-06-05 11:37:06 +0200

    added different @ to classes

--- src/main/java/com/example/BatteryManagement/BatteryRepository.java
+++ src/main/java/com/example/BatteryManagement/BatteryRepository.java
@@ -1,4 +1,7 @@
 package com.example.BatteryManagement;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface BatteryRepository {
 }
--- src/main/java/com/example/BatteryManagement/BatteryService.java
+++ src/main/java/com/example/BatteryManagement/BatteryService.java
@@ -1,4 +1,7 @@
 package com.example.BatteryManagement;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BatteryService {
 }
