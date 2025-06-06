d14d188db1bd634caf53fa4176fb6387bf803bc9
Caspar-Brantberger <3tomu24.branca@folkuniversitetet.nu>
2025-06-06 14:53:16 +0200

    added controller

--- src/main/java/com/example/BatteryManagement/BatteryController.java
+++ src/main/java/com/example/BatteryManagement/BatteryController.java
@@ -1,7 +1,41 @@
 package com.example.BatteryManagement;
 
+import DTO.BatteryInfoResponseDTO;
+import DTO.ChargeResponseDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/battery")
 public class BatteryController {
+
+    private final BatteryService batteryService;
+
+    public BatteryController(BatteryService batteryService) {
+        this.batteryService = batteryService;
+    }
+
+    @GetMapping("/start-optimaztion")
+    public String startOpimization() {
+        new Thread(()-> batteryService.runOptimizedChargingCycle()).start();
+        return "Battery optimization started.";
+    }
+    @GetMapping("/status")
+    public BatteryInfoResponseDTO getCurrentBatteryStatus() {
+        return batteryService.getBatteryInfo();
+    }
+    @PostMapping("/charge/start")
+    public ChargeResponseDTO startCharging(){
+        return batteryService.startCharging();
+    }
+    @PostMapping("/charge/stop")
+    public ChargeResponseDTO stopCharging(){
+        return batteryService.stopCharging();
+    }
+    @PostMapping("/reset")
+    public ChargeResponseDTO resetBatteryToDefault() {
+        return batteryService.resetBatteryToDefault();
+    }
 }
