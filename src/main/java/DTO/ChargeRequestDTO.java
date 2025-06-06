86c837ff9b9cfa6d3cd140a6eece0183504aa07b
Caspar-Brantberger <3tomu24.branca@folkuniversitetet.nu>
2025-06-06 11:52:52 +0200

    ChargeRequestDTO

--- src/main/java/DTO/ChargeRequestDTO.java
+++ src/main/java/DTO/ChargeRequestDTO.java
@@ -1,4 +1,18 @@
 package DTO;
 
 public class ChargeRequestDTO {
+
+    private String charging;
+
+    public ChargeRequestDTO(String charging) {
+        this.charging = charging;
+    }
+    public ChargeRequestDTO() {}
+
+    public String getCharging() {
+        return charging;
+    }
+    public void setCharging(String charging) {
+        this.charging = charging;
+    }
 }
